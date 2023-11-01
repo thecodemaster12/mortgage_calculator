ed8fe07ea025ba52f45dcbb2a3d92ec9aeda4e38
HP <thecodemaster26@gmail.com>
2023-11-01 18:05:44 +0600

    Adding Img

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import { Container,Stack } from '@mui/material'
-import React from 'react'
+import React from 'react';
+// import Pic from "../assets/site.png"
 
 const Footer = () => {
   return (
     <>
-      
+      <h1>Hi</h1>
+      <img src="../assets/site.png" alt="Pic" srcset="" />
     </>
   )
 }
