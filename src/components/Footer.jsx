e702ca1033f043a7f7a6785a651686d79a460bc2
HP <thecodemaster26@gmail.com>
2023-11-01 18:36:38 +0600

    Final Touchup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,18 @@
 import { Container,Stack } from '@mui/material'
 import React from 'react';
-// import Pic from "../assets/site.png"
 
 const Footer = () => {
   return (
     <>
-      <h1>Hi</h1>
-      <img src="../assets/site.png" alt="Pic" srcset="" />
+    <Stack 
+    direction="row"
+    spacing={{ xs: 1, sm: 1.5, md: 2 }}
+    my={{xs: 6, sm: 5, md: 12, lg: 15}}
+    justifyContent="end"
+    >
+      <a href="https://github.com/thecodemaster12" target='_blank'><i class="fa-brands fa-github fa-xl"></i></a>
+      <a href="https://www.linkedin.com/in/saifur-rahman-shihab/" target='_blank'><i class="fa-brands fa-linkedin fa-xl"></i></a>
+    </Stack>
     </>
   )
 }
